6ac754a90d93aafff5f7b4c0a8889fc5cea36004
bit <bit@DESKTOP-DJMLAPN>
2020-05-08 20:45:36 +0900

    commit

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -10,7 +10,7 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("hello World");
 		
 		// TODO Auto-generated method stub
 		//값(value)
